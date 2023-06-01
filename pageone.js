2d779683fe29490fcd45509a2f5359deeb5e6554
ERHolguin <rrodshedale@gmail.com>
2023-06-01 00:30:19 +0000

    Add Pseudocode

--- pageone.js
+++ pageone.js
@@ -1,1 +1,10 @@
-console.log("hello World");
+// Pseudocode
+
+// Get computer choice: random b/t rock, paper
+// scissors. 
+// Create data for player choice.
+// Get player choice. Must be case insensitive.
+// Create round 1 top include player & computer.
+// Return result prompt to declare winner.
+// Create 5-round game that keeps score &
+// report a winner & loser at the end. 
