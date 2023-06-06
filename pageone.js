4bedf01d54191ab3de746841197cfb89d6d29604
ERHolguin <rrodshedale@gmail.com>
2023-06-06 16:22:44 +0000

    Declare Computer & Player Selection

--- pageone.js
+++ pageone.js
@@ -2,15 +2,27 @@
 
 
     function getComputerChoice () { // create funtion for computer choice
-        let computerChoice = ["Rock", "Paper", "Scissors"]; // declare computer choice selectios
+        let computerChoice = ["Rock", "Paper", "Scissors"]; // declare computer choice selections
         return computerChoice[Math.floor(Math.random() * computerChoice.length)] // return results
     }
     
     console.log(getComputerChoice());
 
-// Create data for player choice.
-// Get player choice. Must be case insensitive.
-// Create round 1 top include player & computer.
+    let computerSelection = (getComputerChoice()); // declare computer selection
+    let playerSelection= "ROCK" || "Rock" || "rock"
+    || "PAPER" || "Paper" || "paper" || "SCISSORS" ||
+    "Scissors" || "scissors"; // Declare player choice. Must be case insensitive.
+    
+    let game = prompt("Rock, Paper, Scissors, Shoot!"); //ask player for their guess.    
+
+
+// Create round 1 to include player & computer.
 // Return result prompt to declare winner.
-// Create 5-round game that keeps score &
-// report a winner & loser at the end. 
+     for (i=0; i<5;i++) { // Create 5-round game 
+        if (playerSelection == "ROCK" || "Rock" || "rock") 
+        & computerSelection =="Scissors"{
+
+        }
+     }           
+// Games must keep score 
+// Must report a winner & loser at the end. 
