16869717b4035954db7701b9900b4b78bc552b2b
ERHolguin <rrodshedale@gmail.com>
2023-06-07 16:22:26 +0000

    Add Condition for Draw

--- pageone.js
+++ pageone.js
@@ -8,40 +8,49 @@
     
 //console.log(getComputerChoice()); check point
 
+
 // Declare computer selection.
 // Declare player choice. Must be case insensitive.
-    let computerSelection = (getComputerChoice()); 
+    const computerSelection = (getComputerChoice()); 
 
 // console.log(computerSelection); check point
 
-    let playerSelection = ["ROCK", "Rock", "rock", "PAPER", "Paper", "paper", "SCISSORS", "Scissors", "scissors"];    
-    
-//Ask player for their guess.   
-    let game = prompt("Rock, Paper, Scissors, Shoot!");     
+    const playerSelection = ["ROCK", "Rock", "rock", "PAPER", "Paper", "paper", "SCISSORS", "Scissors", "scissors"]; 
+
+
+    //Ask player for their guess.   
+    let game = prompt("Rock, Paper, Scissors, Shoot!"); 
 
 // Create round 1 to include player & computer.
 // Create 5-round game. 
+
      for(i=0; i<5;i++) { 
-        if(playerSelection == "ROCK" || "Rock" || "rock" && computerSelection =="Scissors"){
+        if(playerSelection === computerSelection) {
+            alert("It's a tie! Please try again");
+        }
+        if(playerSelection === "ROCK" || "Rock" || "rock" && computerSelection ==="Scissors"){
             alert("You win! Rock beats Scissors");
-        }else if
-            (playerSelection == "ROCK" || "Rock" || "rock" && computerSelection =="Paper"){
+        }
+        else if(playerSelection === "ROCK" || "Rock" || "rock" && computerSelection ==="Paper"){
             alert("You lose! Paper beats Rock");
-        }else if
-            (playerSelection == "PAPER" || "Paper" || "paper" && computerSelection =="Rock"){
+        }
+        else if(playerSelection === "PAPER" || "Paper" || "paper" && computerSelection ==="Rock"){
             alert("You win! Paper beats Rock");
-        }else if
-            (playerSelection == "PAPER" || "Paper" || "paper" && computerSelection =="Scissors"){
+        }
+        else if(playerSelection === "PAPER" || "Paper" || "paper" && computerSelection ==="Scissors"){
             alert("You lose! Scissors beats Paper");
-        }else if
-        (playerSelection == "SCISSORS"|| "Scissors"|| "scissors" && computerSelection =="paper"){
+        }
+        else if(playerSelection === "SCISSORS"|| "Scissors"|| "scissors" && computerSelection ==="paper"){
             alert("You win! Scissors beats Paper");
-        }else if
-        (playerSelection == "SCISSORS"|| "Scissors"|| "scissors" && computerSelection =="rock"){
+        }
+        else if(playerSelection === "SCISSORS"|| "Scissors"|| "scissors" && computerSelection ==="rock"){
             alert("You lose! Rock beats Scissors");
-        }else 
-            alert("It's a tie! Please try again.");
-        }           
+        }
+        else {
+            alert("Invalid Input");
+        }   
+        
+    }
 //console.log(i); check point
 // Return result prompt to declare winner.
 // Games must keep score. 
