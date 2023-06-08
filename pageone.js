16b4aca66b1e6fa2e2d02b7f829cf1ab6a1bc95d
ERHolguin <rrodshedale@gmail.com>
2023-06-08 19:29:02 +0000

    Redefine Player & Computer Selections

--- pageone.js
+++ pageone.js
@@ -1,7 +1,19 @@
 // Create funtion for computer choice; declare computer choice selections; return results.
-    function getComputerChoice () { 
+    function getComputerChoice() { 
         let computerChoice = ["Rock", "Paper", "Scissors"]; 
         return computerChoice[Math.floor(Math.random() * computerChoice.length)] 
     }
-    
+   //console.log(getComputerChoice());
 
+// Write a function that plays a single round of Rock Paper Scissors. 
+    function playRound(playerSelection, computerSelection) {
+        return (playerSelection, computerSelection);
+
+    }
+    
+        const playerSelection = "rock" || "paper" || "scissors";
+        const computerSelection = getComputerChoice();
+    //console.log(playRound(playerSelection, computerSelection));
+
+   
+//Make function's playerSelection parameter case-insensitive
