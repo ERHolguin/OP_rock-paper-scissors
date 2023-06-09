ae13e7d99b2274e14a0dc254038cd2311cc30345
ERHolguin <rrodshedale@gmail.com>
2023-06-09 20:19:28 +0000

    Fix Syntax Issue Causing Player to Win all the Time

--- pageone.js
+++ pageone.js
@@ -1,22 +1,41 @@
-// Create funtion for computer choice; declare computer choice selections; return results.
+    // Create a funtion to generate computer choice.
     function getComputerChoice() { 
-        let computerChoice = ["Rock", "Paper", "Scissors"]; 
+        let computerChoice = ["rock", "paper", "scissors"]; 
         return computerChoice[Math.floor(Math.random() * computerChoice.length)] 
     }
-   //console.log(getComputerChoice());
+        //console.log(getComputerChoice());
 
-// Write a function that plays a single round of Rock Paper Scissors. 
+    // Request player input + make function's playerSelection parameter case-insensitive  
+    const playerSelection = prompt("Rock, Paper, Scissors, Shoot!");
+    const computerSelection = getComputerChoice(); 
+
+    //Write a function that plays a single round of Rock Paper Scissors with player + computer.
     function playRound(playerSelection, computerSelection) {
-        return (playerSelection, computerSelection);
 
     }
-    
-        const playerSelection = "rock" || "paper" || "scissors";
-        const computerSelection = getComputerChoice();
-    //console.log(playRound(playerSelection, computerSelection));
-    //console.log(playerSelection);
 
-   
-//Make function's playerSelection parameter case-insensitive
-        const caseInsensitive = playerSelection.toLowerCase();
-//console.log(caseInsensitive);
+                //Define game conditions.
+                if (playerSelection === computerSelection) {
+                    alert("It's a tie! Please try again");
+            } else if(playerSelection === "rock" && computerSelection ==="scissors") {
+                    alert("You win! Rock beats Scissors");
+            } else if(playerSelection === "rock" && computerSelection ==="paper") {
+                    alert("You lose! Paper beats Rock");
+            } else if(playerSelection === "paper" && computerSelection ==="rock") { 
+                    alert("You win! Paper beats Rock");
+            } else if(playerSelection === "paper" && computerSelection ==="scissors") {
+                    alert("You lose! Scissors beat Paper");
+            } else if(playerSelection === "scissors" && computerSelection ==="paper") {
+                    alert("You win! Scissors beat Paper");
+            } else if(playerSelection === "scissors" && computerSelection ==="rock") {
+                    alert("You lose! Rock beats Scissors");
+            } else {
+                    alert("Invalid Input");
+            }
+
+    console.log (`${playerSelection}, ${computerSelection}`);
+
+
+
+
+    
