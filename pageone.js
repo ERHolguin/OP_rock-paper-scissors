73688f59d0e0fd9d7b591c52e7b44e1dcbe6eefe
ERHolguin <rrodshedale@gmail.com>
2023-06-21 21:24:23 +0000

    Create 5-Round Game and Fix Syntax

--- pageone.js
+++ pageone.js
@@ -6,43 +6,44 @@
         //console.log(getComputerChoice());
 
     //Write a function that plays a single round of Rock Paper Scissors with player + computer.
-    function playRound(playerSelection, computerSelection) {
-        console.log(playerSelection + ', ' + computerSelection);
-    }
-        // Request player input + make function's playerSelection parameter case-insensitive  
-        let userInput = prompt("Rock, Paper, Scissors, Shoot!");
-        let playerSelection = userInput?.toLowerCase();
-        let computerSelection = getComputerChoice();
-
-        playRound(playerSelection, computerSelection);
-    //Define game conditions.
-        
-                if (playerSelection === computerSelection) {
-                    alert("It's a tie! Please try again");
+    function playRound() {
+        let userInput = prompt('Rock, Paper, Scissors, Shoot!'); // Request player input  
+        const playerSelection = userInput?.toLowerCase(); // Make function's playerSelection parameter case-insensitive. Used chaining operator to throw off null errors.
+        const computerSelection = getComputerChoice();
+            //Define game conditions.                            
+            if (playerSelection === computerSelection) {
+            return "It's a tie! Please try again";
             } else if(playerSelection === "rock" && computerSelection ==="scissors") {
-                    alert("You win! Rock beats Scissors");
+            return "You win! Rock beats Scissors";
             } else if(playerSelection === "rock" && computerSelection ==="paper") {
-                    alert("You lose! Paper beats Rock");
+            return "You lose! Paper beats Rock";
             } else if(playerSelection === "paper" && computerSelection ==="rock") { 
-                    alert("You win! Paper beats Rock");
+            return "You win! Paper beats Rock";
             } else if(playerSelection === "paper" && computerSelection ==="scissors") {
-                    alert("You lose! Scissors beat Paper");
+            return "You lose! Scissors beat Paper";
             } else if(playerSelection === "scissors" && computerSelection ==="paper") {
-                    alert("You win! Scissors beat Paper");
+            return "You win! Scissors beat Paper";
             } else if(playerSelection === "scissors" && computerSelection ==="rock") {
-                    alert("You lose! Rock beats Scissors");
+            return "You lose! Rock beats Scissors";
             } else {
-                    alert("Invalid Input");
+                return "Invalid Input";
             }
-       
-        //console.log (`${playerSelection}, ${computerSelection}`);
+    }     
+        // Create a 5-round game                   
+        function game() {
+            for (let i = 0; i < 5; i++){
+                console.log(playRound());
+            }
+        }
 
-        // Create 5-round game.
-            //for (let i = 0; i <5; i++) {
-                //console.log(playRound(), i);            
-            //}
-  
-    // Return result prompt to declare winner.
+        game(); // Call the function to initiate the game. 
+    
+ 
+
+    
+                
+
+
     // Games must keep score.
     // Must report a winner & loser at the end.
 
