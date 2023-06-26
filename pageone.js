406d4905552a33d9f2942d6ef2050d9124080d6a
ERHolguin <rrodshedale@gmail.com>
2023-06-26 17:11:58 +0000

    Fix Bug in Winning Condition for Player

--- pageone.js
+++ pageone.js
@@ -24,31 +24,31 @@
             //Define game conditions.                            
             if (playerSelection === computerSelection) {
                 updateScore();
-                return "It's a tie! Please try again";
+                return "It's a tie! Please try again.";
             } else if(playerSelection === "rock" && computerSelection ==="scissors") {
                 playerScore += 1;
                 computerScore += 0;
-                return "You win! Rock beats Scissors";
+                return "You win! Rock beats Scissors.";
             } else if(playerSelection === "rock" && computerSelection ==="paper") {      
                 playerScore += 0;
                 computerScore += 1;
-                return "You lose! Paper beats Rock";
+                return "You lose! Paper beats Rock.";
             } else if(playerSelection === "paper" && computerSelection ==="rock") { 
                 playerScore += 1;
                 computerScore += 0;
-                return "You win! Paper beats Rock";
+                return "You win! Paper beats Rock.";
             } else if(playerSelection === "paper" && computerSelection ==="scissors") {
                 playerScore += 0;
                 computerScore += 1;
-                return "You lose! Scissors beat Paper";
+                return "You lose! Scissors beat Paper.";
             } else if(playerSelection === "scissors" && computerSelection ==="paper") {
                 playerScore += 1;
                 computerScore += 0;
-                return "You win! Scissors beat Paper";
+                return "You win! Scissors beat Paper.";
             } else if(playerSelection === "scissors" && computerSelection ==="rock") {
                 playerScore += 0;
                 computerScore += 1;
-                return "You lose! Rock beats Scissors";
+                return "You lose! Rock beats Scissors.";
             } else {
                 return "Invalid Input";
             }
@@ -64,12 +64,12 @@
 
         game(); // Call the function to initiate the game.
         updateScore();
-        if (playerScore === 3) {
+        if (playerScore >= 3) {
             console.log("You win! Human beats Robot!");
             } else if(playerScore === 2 && computerScore === 2) {
             console.log("It's a tie! Would you like to try again?");
             } else {
-            console.log("You lose! Robot beats Human");
+            console.log("You lose! Robot beats Human.");
         } 
           
 
