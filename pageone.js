cae35fe68d4bcb0ba6dd1f2e6be4ecc9402a7cb6
ERHolguin <rrodshedale@gmail.com>
2023-10-16 17:13:07 +0000

    End Game Once a Player Scores 5 Points

--- pageone.js
+++ pageone.js
@@ -3,8 +3,6 @@
         return computerChoice[Math.floor(Math.random() * computerChoice.length)] 
     }
 
-    let clickCount = 0;
-    let clickCountDisplay = document.getElementById('clickCount');
     const playerButtons = document.querySelectorAll('button');
         playerButtons.forEach((button) => {
             button.addEventListener('click', playRound)
@@ -12,32 +10,42 @@
         playerButtons.forEach((button) => {
             button.addEventListener('click', updateScore)
             });
-
-
-        /*playerButtons.forEach((button) => {
-            button.removeEventListener('click', playRound)
-            });
-        playerButtons.forEach((button) => {
-            button.removeEventListener('click', updateScore)
-            });*/
         
     let playerScore = 0;
     let computerScore = 0;
 
    function updateScore() {
+    let resultMessage = document.getElementById('resultMessage');
+    const playerScorePara = document.getElementById('playerScorePara').innerText = playerScore;
+    const computerScorePara = document.getElementById('computerScorePara').innerText = computerScore;
         playerScore += 0;
-            //console.log('Player Score: ', playerScore);
         computerScore += 0;
-            //console.log('Computer Score: ', computerScore);
-    }
+            if (playerScore === 5) {
+                playerButtons.forEach((button) => {
+                    button.disabled = true });
+                playerButtons.forEach((button) => {
+                    button.removeEventListener('click', playRound)
+                    });
+                playerButtons.forEach((button) => {
+                    button.removeEventListener('click', updateScore)
+                    });
+                resultMessage.innerText = 'You win! Human beats Robot!';
+            } else if (computerScore === 5) {
+                playerButtons.forEach((button) => {
+                    button.disabled = true });
+                playerButtons.forEach((button) => {
+                    button.removeEventListener('click', playRound)
+                    });
+                playerButtons.forEach((button) => {
+                    button.removeEventListener('click', updateScore)
+                    });
+                resultMessage.innerText ='You lose! Robot beats Human.';
+            } 
+        }
 
     function playRound(e) {
         const computerSelection = getComputerChoice();
         const playerSelection = e.target.id
-        const playerScorePara = document.getElementById('playerScorePara').innerText = playerScore;
-        const computerScorePara = document.getElementById('computerScorePara').innerText = computerScore;
-        clickCount++;
-        clickCountDisplay.innerText = clickCount;
 
             if (playerSelection === computerSelection) {
                 updateScore();
@@ -71,44 +79,17 @@
             }
             
     }
-    /*
-    function endGame(e) {
-        if (clickCount === clicks) {
-            playerButtons.forEach((button) => {
-            button.removeEventListener('click', playRound)
-            });
-        playerButtons.forEach((button) => {
-            button.removeEventListener('click', updateScore)
-            });
-        }
-    }
-
-    removeEventListenerAfterClicks(button, 'click', endGame, 5);
 
 
-        // give results
-                if (playerScore >= 3) {
-            console.log('You win! Human beats Robot!');
-            } else if(playerScore === 2 && computerScore === 2) {
-            console.log('It/s a tie! Would you like to try again?');
-            } else if (computerScore === 3) {
-            console.log('You lose! Robot beats Human.');
-        // then give player option to retsart the game
-        // reset score updater to zero 
+   /* 
 
-        } 
-    }
+   what needs to happen next?
+// give final results & announce a winner
+// then disbale player buttons 
+// then give player option to restart the game
+// reset score updater to zero
 
-
-        // Create a 5-round game                   
-        function game() {
-            for (let i = 0; i < 5; i++){
-                console.log(playRound());
-            }
-
-        } 
-
-        game(); */
+*/
 
 
           
